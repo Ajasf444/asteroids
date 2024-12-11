776159df665acff12baaeb3b453ecd3d7d2b04c9
Ajasf444 <longstephen444@gmail.com>
2024-12-11 16:15:59 -0600

    player movement

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill(color = (0, 0, 0))
         player.draw(screen)
         pygame.display.flip()
 
         # limit framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
 
 if __name__ == '__main__':                                      
     main()
