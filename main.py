58a91072e891fe6cfedb0bdef812cefcc6956487
Ajasf444 <longstephen444@gmail.com>
2024-12-11 22:54:05 -0600

    shots and asteroids destroyed when colliding

--- main.py
+++ main.py
@@ -36,6 +36,10 @@ def main():
             if asteroid.collides_with(player):
                 print('Game Over!')
                 return
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.kill()
+                    shot.kill()
 
         screen.fill(color = (0, 0, 0))
 
